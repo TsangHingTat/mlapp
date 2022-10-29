c8287c9e7249548338c83ed50eb4dc5beb3e4964
TsangHingTat <hingtattsang@gmail.com>
2022-10-30 03:41:46 +0800

    update

---
+++ MLApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MLApp
+//
+//  Created by Hing Tat Tsang on 29/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MLApp/MLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLAppApp.swift
+//  MLApp
+//
+//  Created by Hing Tat Tsang on 29/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct MLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
